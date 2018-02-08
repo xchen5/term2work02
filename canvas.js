9168c9b7dacd314a6e0544e7b6e6762e0f7dae07
Xavier Chen <xchen5@stuy.edu>
2018-02-07 19:27:53 -0500

    Finished

--- canvas.js
+++ canvas.js
@@ -1,30 +1,42 @@
 c = document.getElementById("can");
+startButton = document.getElementById("start");
+stopButton = document.getElementById("stop");
 var width = c.width;
 var height = c.height;
 var requestID;
-var x = 0;
 var ctx = c.getContext('2d');
-var toDraw = function(){
-    ctx.clearRect(0,0,500,500);
-    ctx.beginPath();
-    ctx.arc(width/2,height/2,x,0, 2 * Math.PI);
-    ctx.fill();
-    toShrink = 0;
-    if (toShrink === 0){
-	x+=1;
-	if (x === 250){
-	    toShrink = 1;
-	};
-    }
-    else if(toShrink === 1){
-	x-=1;
-	if( x === 1 ){
-	    toShrink = 0;
-	};
+var x = 0;
+var toEnlarge = true;
+var draw = function(){
+  window.cancelAnimationFrame(requestID);
+  ctx.clearRect(0,0,500,500);
+  ctx.beginPath();
+  ctx.arc(width/2,height/2,x,0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  if (toEnlarge){
+    x+=1;
+    if (x >= 250){
+      toEnlarge = false;
     };
-    requestID = window.requestAnimationFrame(toDraw);
-    console.log(requestID);
+  }
+  else{
+    x-=1;
+    if( x <= 1 ){
+      toEnlarge = true;
+    };
+  };
+  requestID = window.requestAnimationFrame(toDraw);
+  console.log(requestID);
 };
 
-window.requestAnimationFrame(toDraw);
+var toDraw = function(){
+  window.requestAnimationFrame(draw);
+}
 
+var toStop = function(){
+  window.cancelAnimationFrame(requestID);
+}
+
+stopButton.addEventListener("click", toStop);
+startButton.addEventListener("click", draw);
